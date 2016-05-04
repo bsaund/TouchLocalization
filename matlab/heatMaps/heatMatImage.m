10f9d9fdaef3a2c681665202e60c987d88881cff
Brad Saund <brad.saund@gmail.com>
2016-05-04 15:38:51 -0400

    altered heatMap for dealing with large files

--- matlab/heatMaps/heatMatImage.m
+++ matlab/heatMaps/heatMatImage.m
@@ -1,18 +1,38 @@
-data =csvread('example.txt');
-x = data(:,1);
-y = data(:,2);
-z = data(:,3);
+directory = './data/topToFront/';
+files = dir(directory);
 
-x = x(1:numel(unique(x))); 
-y = y(1:numel(unique(y))); 
+% fig = figure();
 
-% imagesc(x,y,z)
-img = reshape(z, size(y,1), size(x,1));
-img = flipud(img)
-img = fliplr(img)
-imagesc(img)
-colormap('hot');
-set(gca,'dataAspectRatio',[1 1 1])
+
+F = [];
+for imgNum = 1:size(files,1)-2
+    data =csvread(['./data/topToFront/', files(imgNum+2).name]);
+    x = data(:,1);
+    y = data(:,2);
+    z = data(:,3);
+    
+    x = x(1:numel(unique(x)));
+    y = y(1:numel(unique(y)));
+    
+    % imagesc(x,y,z)
+    img = reshape(z, size(y,1), size(x,1))';
+    img = flipud(img);
+    img = fliplr(img);
+    F(:,:,imgNum) = img;
+%     iHd = imshow(img,'InitialMagnification','fit')
+    
+%     truesize(fig, [size(x,1) size(y,1)]*2);
+%     colormap('hot');
+%     colorbar
+%     set(gca,'dataAspectRatio',[1 1 1])
+%     drawnow
+end
+
+
+F = F/max(max(max(F)));
+implay(F)
+% immovie(F)
+
 
 % size(data)
 % 
